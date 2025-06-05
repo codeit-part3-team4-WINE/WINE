5d27dd0d9b3179a3a4000990aac2ae0f91664238
Yongmin0423 <yym21345@gmail.com>
2025-06-05 13:45:36 +0900

    #10 refactor: 랜딩 페이지 원복

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import InputPair from '@/components/Inputs/InputPair';
-import SearchInput from '@/components/Inputs/SearchInput';
-
 export default function LandingPage() {
-  return (
-    <h1 className='text-primary-100 text-3xl'>
-      <SearchInput type='text' />
-      <InputPair label='와인 이름' />
-    </h1>
-  );
+  return <h1 className='text-primary-100 text-3xl'>Landing</h1>;
 }
