0567532c5f30a790664f771abb052a3b87c42bd4
김태일 <ejrrhcl56@naver.com>
2025-06-05 14:42:50 +0900

    [#11] feat: footer 구현 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Footer from '@/components/Footer';
+
 export default function LandingPage() {
-  return <h1 className='text-primary-100 text-3xl'>Landing</h1>;
+  return (
+    <>
+      <h1 className='text-primary-100 text-3xl'>Landing</h1>
+      <Footer />
+    </>
+  );
 }
