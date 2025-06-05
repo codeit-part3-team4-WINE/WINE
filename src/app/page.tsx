20f68b0d5b55573d72681efdfea545f2d2d29dd7
Yongmin0423 <yym21345@gmail.com>
2025-06-05 14:26:49 +0900

    #10 fix: test를 위한 landing페이지 원복

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import InputPair from '@/components/Inputs/InputPair';
-import SearchInput from '@/components/Inputs/SearchInput';
-
 export default function LandingPage() {
-  return (
-    <h1 className='text-primary-100 text-3xl'>
-      <InputPair label='와인 인름' />
-      <SearchInput />
-    </h1>
-  );
+  return <h1 className='text-primary-100 text-3xl'>Landing</h1>;
 }
