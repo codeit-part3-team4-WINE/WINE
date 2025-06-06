029a7941487deb07eed45fb3102752a2f4107524
Yongmin0423 <yym21345@gmail.com>
2025-06-07 03:07:45 +0900

    [#10] fix: 잘못된 파일 첨부 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import wineImage from '@/app/assets/Images/image.png';
-import WineInfo from '@/components/WineInfo';
-
 export default function LandingPage() {
-  // return <h1 className='text-primary-100 text-3xl'>Landing</h1>;
-  return <WineInfo wineImage={wineImage} />;
+  return <h1 className='text-primary-100 text-3xl'>Landing</h1>;
 }
