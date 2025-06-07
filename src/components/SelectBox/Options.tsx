35ad9f41ac3a3983136c2995c8ae3adeb8d85afa
김서연 <rainbow2870@naver.com>
2025-06-07 15:29:27 +0900

    [#30] feat(selectbox): 옵션 리 컴포넞트 Options 구현 - 열림 상태에 따른 조건부 렌더링 적용

---
+++ src/components/SelectBox/Options.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { ReactNode } from 'react';
+
+import { cn } from '@/libs/cn';
+
+import { useSelectedBoxContext } from './SelectBoxContext';
+
+export default function Options({
+  children,
+  optionsClassName,
+}: {
+  children: ReactNode;
+  optionsClassName?: string;
+}) {
+  const { isOpen } = useSelectedBoxContext();
+
+  if (!isOpen) return null;
+
+  return (
+    <ul
+      className={cn(
+        'absolute z-10 mt-2 w-[32.7rem] rounded-[1.6rem] border border-gray-300 md:w-[41.2rem]',
+        optionsClassName,
+      )}
+    >
+      {children}
+    </ul>
+  );
+}
