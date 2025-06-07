e872b6188eb6beadf570962e6663c50512b87d8e
김서연 <rainbow2870@naver.com>
2025-06-07 15:33:26 +0900

    [#30] feat(selectbox): commonselectbox를 합성 컴포넌트로 구성하여 trigger,options,option 컴포넌트와 연결

---
+++ src/components/SelectBox/index.ts
@@ -0,0 +1,10 @@
+import Option from './Option';
+import Options from './Options';
+import SelectBox from './SelectBox';
+import Trigger from './Trigger';
+
+export const CommonSelectBox = Object.assign(SelectBox, {
+  Trigger,
+  Options,
+  Option,
+});
