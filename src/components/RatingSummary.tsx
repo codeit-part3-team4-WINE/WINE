4cf15cac7a8e077ba8ca6328cc4ba2baf3046399
MyungJiwoo <1206jiwoo@gmail.com>
2025-06-08 23:07:23 +0900

    [#35] refactor: ratingSummary 컴포넌트를 합성 컴포넌트 패턴으로 리팩토링

--- src/components/RatingSummary.tsx
+++ src/components/RatingSummary.tsx
@@ -1,13 +1,8 @@
 import StarIcon from '@/app/assets/icons/star';
 import { cn } from '@/libs/cn';
 
-interface RatingSummaryContextType {
-  rating: number;
-  totalReviews?: number | null;
-  description?: string | null;
-  direction: 'row' | 'col';
-  size: 'sm' | 'md' | 'lg';
-}
+type Direction = 'row' | 'col';
+type Size = 'sm' | 'md' | 'lg';
 
 const SIZE_VARIANTS = {
   lg: {
@@ -31,79 +26,96 @@ const getFillPercent = (rating: number) => {
   return rating % 1 === 0 ? 100 : (rating % 1) * 100;
 };
 
-export default function RatingSummary({
+function RatingSummary({
   rating,
-  totalReviews = null,
-  description = null,
   direction = 'col',
-  size,
-}: RatingSummaryContextType) {
-  const {
-    rating: ratingSize,
-    icon: iconSize,
-    text: textSize,
-  } = SIZE_VARIANTS[size];
+  size = 'md',
+  iconColor = '#6A42DB',
+  className,
+  children,
+}: {
+  rating: number;
+  direction?: Direction;
+  size?: Size;
+  iconColor?: string;
+  className?: string;
+  children?: React.ReactNode;
+}) {
+  const ratingSize = SIZE_VARIANTS[size].rating;
 
-  const Stars = (
-    <div className='relative flex'>
-      <div className='flex'>
-        {/* 별점 배경 (빈 별) */}
-        {Array.from({ length: 5 }, (_, i) => (
-          <StarIcon key={`empty-${i}`} filled size={iconSize} />
-        ))}
-
-        {/* 별점 (채워진 별) */}
-        <div className='absolute flex'>
-          {Array.from({ length: rating - 1 }, (_, i) => (
-            <StarIcon
-              key={`filled-${i}`}
-              filled
-              color='#6A42DB'
-              size={iconSize}
-            />
-          ))}
-
-          {/* 소수점 단위 표현 */}
-          <StarIcon
-            key='filled-last'
-            filled
-            color='#6A42DB'
-            fillPercent={getFillPercent(rating)}
-            size={iconSize}
-          />
-        </div>
-      </div>
-    </div>
-  );
-
-  const Texts = (
-    <div className='w-[10rem]'>
-      {totalReviews && (
-        <p className={cn('content-text text-gray-500', textSize)}>
-          {totalReviews}개의 후기
-        </p>
-      )}
-      {description && (
-        <p className={cn('content-text text-gray-500', textSize)}>
-          {description}
-        </p>
-      )}
-    </div>
+  const Content = (
+    <>
+      <Stars color={iconColor} rating={rating} size={size} />
+      {children}
+    </>
   );
 
   return direction === 'col' ? (
-    <div className='flex flex-col items-start gap-[1rem]'>
+    <div className={cn('flex flex-col items-start gap-[1rem]', className)}>
       <p className={cn('font-bold', ratingSize)}>{rating.toFixed(1)}</p>
-      {Stars}
-      {Texts}
+      {Content}
     </div>
   ) : (
-    <div className='flex items-center gap-[2rem]'>
+    <div className={cn('flex items-center gap-[2rem]', className)}>
       <p className={cn('font-bold', ratingSize)}>{rating.toFixed(1)}</p>
-      <div className='flex flex-col items-start gap-[1rem]'>
-        {Stars}
-        {Texts}
+      <div className='flex flex-col items-start gap-[1rem]'>{Content}</div>
+    </div>
+  );
+}
+
+function Stars({
+  rating,
+  size = 'md',
+  color = '#6A42DB',
+}: {
+  rating: number;
+  size?: Size;
+  color?: string;
+}) {
+  const max = 5;
+  const iconSize = SIZE_VARIANTS[size].icon;
+  const fullStars = Math.floor(rating);
+
+  return (
+    <div className='relative flex'>
+      {/* 빈 별 */}
+      {Array.from({ length: max }, (_, i) => (
+        <StarIcon key={`empty-${i}`} filled size={iconSize} />
+      ))}
+      {/* 채워진 별 */}
+      <div className='pointer-events-none absolute flex'>
+        {Array.from({ length: fullStars }, (_, i) => (
+          <StarIcon key={`filled-${i}`} filled color={color} size={iconSize} />
+        ))}
+        <StarIcon
+          key='filled-last'
+          filled
+          color={color}
+          fillPercent={getFillPercent(rating)}
+          size={iconSize}
+        />
       </div>
     </div>
   );
 }
+
+function Text({
+  children,
+  size = 'md',
+  className,
+}: {
+  children: React.ReactNode;
+  size?: Size;
+  className?: string;
+}) {
+  const textSize = SIZE_VARIANTS[size].text;
+  return (
+    <p className={cn('w-[10rem] text-gray-500', textSize, className)}>
+      {children}
+    </p>
+  );
+}
+RatingSummary.Stars = Stars;
+RatingSummary.Text = Text;
+
+export default RatingSummary;
