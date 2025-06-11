b5c89815f8f7ca832f7260de237be7af3ca7df36
김서연 <rainbow2870@naver.com>
2025-06-11 20:23:01 +0900

    [#65] refactor(wincard): 와인 크지 조정 및 텍스트 구역 크기 조정

--- src/app/myprofile/components/Card/WineCard.tsx
+++ src/app/myprofile/components/Card/WineCard.tsx
@@ -15,10 +15,10 @@ import CardDropdown from './CardDropdown';
 export default function WineCard({ name, region, image, price }) {
   return (
     <article className='relative mt-10 flex h-[16rem] w-full cursor-pointer rounded-[1.2rem] border border-gray-300 px-[2rem] py-[2rem] md:mt-20 md:h-[22.8rem]'>
-      <div className='absolute -top-8 left-[2rem] h-[18rem] w-[5.3rem] md:-top-17 md:h-[27rem] md:w-[7rem]'>
+      <div className='absolute -top-8 left-[2rem] h-[18rem] w-[5.3rem] md:-top-17 md:h-[27rem] md:w-[8rem]'>
         <Image fill alt={name} className='object-cover' src={image} />
       </div>
-      <div className='ml-[7rem] flex h-full flex-col justify-center gap-y-2 md:ml-[14rem] md:w-[30rem] md:gap-y-5'>
+      <div className='ml-[7rem] flex h-full w-[19rem] flex-col justify-center gap-y-2 md:ml-[14rem] md:w-[30rem] md:gap-y-5'>
         <h1 className='text-xl font-bold text-gray-800 md:text-[3rem]'>
           {name}
         </h1>
