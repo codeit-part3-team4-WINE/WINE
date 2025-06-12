96977bfe313e2f3f23decf100c1104accc00c729
MyungJiwoo <1206jiwoo@gmail.com>
2025-06-12 23:29:08 +0900

    [#79] feat: 전역 페이지 레이아웃 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,15 @@ export default function RootLayout({
   return (
     <html lang='ko'>
       <body>
-        <Header />
-        <QueryProvider>{children}</QueryProvider>
-        <Footer />
+        <header className='mx-auto w-full max-w-[350rem] px-[5vw] md:px-[10vw] xl:px-[15vw]'>
+          <Header />
+        </header>
+        <main className='mx-auto my-10 w-full max-w-[350rem] px-[5vw] md:px-[10vw] xl:px-[15vw]'>
+          <QueryProvider>{children}</QueryProvider>
+        </main>
+        <footer className='w-screen'>
+          <Footer />
+        </footer>
       </body>
     </html>
   );
