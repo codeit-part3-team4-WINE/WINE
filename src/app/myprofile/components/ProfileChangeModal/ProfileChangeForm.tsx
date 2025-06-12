9e58a169bd09cfd5012c74f9cd6f2f79713eac89
김서연 <rainbow2870@naver.com>
2025-06-12 16:31:31 +0900

    [#81] feat: 프로필 삭제 기능 추가

--- src/app/myprofile/components/ProfileChangeModal/ProfileChangeForm.tsx
+++ src/app/myprofile/components/ProfileChangeModal/ProfileChangeForm.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { StaticImageData } from 'next/image';
 import { useState } from 'react';
 
 import DefaultImg from '@/app/assets/svgs/profile-default.svg';
+import MockImg from '@/app/design-system/ui-profileImg/mock-image/mock-img.png';
 import Button from '@/components/Button';
 import InputFile from '@/components/Inputs/InputFile';
 import InputPair from '@/components/Inputs/InputPair';
@@ -10,10 +12,11 @@ import ProfileImg from '@/components/ProfileImg';
 
 const user = {
   name: 'WHYNE',
+  image: MockImg,
 };
 
 export default function ProfileChangeFrom() {
-  const [imgSrc, setImgSrc] = useState('');
+  const [imgSrc, setImgSrc] = useState<string | StaticImageData>(user.image);
   const placeholder = user.name;
 
   return (
@@ -23,10 +26,29 @@ export default function ProfileChangeFrom() {
           <ProfileImg isSelectable size='lg' src={imgSrc || undefined} />
         </InputFile>
         <span className='text-md text-gray-500'>
-          {imgSrc === '' || imgSrc === DefaultImg
+          {imgSrc === user.image || imgSrc === DefaultImg
             ? '프로필 사진 변경'
             : '미리보기 이미지'}
         </span>
+        <div className='flex gap-[0.3rem]'>
+          <Button
+            className='text-[1rem]'
+            size='xs'
+            variant='ghost'
+            onClick={() => setImgSrc(DefaultImg)}
+          >
+            프로필 사진 삭제
+          </Button>
+
+          <Button
+            className='text-[1rem]'
+            size='xs'
+            variant='ghost'
+            onClick={() => setImgSrc(user.image)}
+          >
+            기존 이미지로 되돌리기
+          </Button>
+        </div>
       </div>
 
       <div className='flex flex-col items-end gap-8'>
@@ -35,15 +57,6 @@ export default function ProfileChangeFrom() {
           label='닉네임'
           placeholder={placeholder}
         />
-
-        <Button
-          className='text-[1rem]'
-          size='xs'
-          variant='ghost'
-          onClick={() => setImgSrc(DefaultImg)}
-        >
-          기본 이미지로 변경
-        </Button>
       </div>
     </form>
   );
