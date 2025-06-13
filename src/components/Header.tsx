3b18d9b487447fc1ae8ac921f153887040ad7198
evaain706 <evaain706@gmail.com>
2025-06-13 15:39:16 +0900

    [#94] style: 헤더 로그인시 조건부 스타일링

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,41 +4,75 @@ import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 import WineLogoIcon from '@/app/assets/icons/wine-logo';
+import useLogout from '@/hooks/useLogout';
+import useUserStore from '@/stores/Auth-store/authStore';
+
+import Dropdown from './Dropdown';
+import ProfileImg from './ProfileImg';
 
 export default function Header() {
-  // const [isLogin, setIsLogin] = useState(false); //추후에 zustand store에서 state를 가져와 User정보 유무에 따라 조건부렌더링으로 수정
   const router = useRouter();
+  const user = useUserStore((state) => state.user);
+
+  const logout = useLogout();
+  const setUser = useUserStore((state) => state.setUser);
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      setUser(null);
+    } catch (error) {
+      alert('로그아웃 실패');
+      console.error('로그아웃 실패:', error);
+    }
+  };
 
   return (
-    <div className=''>
+    <div>
       <header className='mt-7 h-[5rem] rounded-2xl bg-[#101318] text-white md:h-[7rem]'>
         <div className='flex h-full items-center justify-between px-[4rem] md:px-[8rem]'>
           <Link aria-label='홈으로 이동' href='/'>
             <WineLogoIcon className='cursor-pointer' />
           </Link>
 
-          <div className='flex items-center space-x-8 text-[1.4rem] md:text-[1.6rem] lg:space-x-20'>
-            {/* {isLogin ? (
-              <div className='h-[3rem] w-[3rem] rounded-full bg-white md:h-[4.5rem] md:w-[4.5rem]' />
+          <div className='flex items-center justify-center space-x-8 text-[1.4rem] md:text-[1.6rem] lg:space-x-20'>
+            {user ? (
+              <div className='text-gray-700'>
+                <Dropdown>
+                  <Dropdown.Trigger>
+                    <ProfileImg
+                      className='mt-3 inline-block h-[3rem] w-[3rem] align-middle hover:animate-pulse md:h-[4rem] md:w-[4rem]'
+                      src={user?.image ?? undefined}
+                    />
+                  </Dropdown.Trigger>
+                  <Dropdown.Menu>
+                    <Dropdown.Item onClick={handleLogout}>
+                      로그아웃
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={() => router.push(`/myprofile`)}>
+                      마이페이지
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
             ) : (
-              // 위의 div대신 아바타 컴포넌트 들어가기
               <>
                 <p
-                  onClick={() => router.push(`/login`)}
                   className='cursor-pointer transition-colors hover:text-gray-600'
+                  onClick={() => router.push(`/login`)}
                 >
                   로그인
                 </p>
                 <p
-                  onClick={() => router.push(`/signup`)}
                   className='cursor-pointer transition-colors hover:text-gray-600'
+                  onClick={() => router.push(`/signup`)}
                 >
                   회원가입
                 </p>
               </>
-            )} */}
+            )}
 
-            <button
+            {/* <button
               className='cursor-pointer transition-colors hover:text-gray-600'
               onClick={() => router.push(`/login`)}
             >
@@ -49,7 +83,7 @@ export default function Header() {
               onClick={() => router.push(`/signup`)}
             >
               회원가입
-            </button>
+            </button> */}
           </div>
         </div>
       </header>
