8f2b253b511019ac65f065a495429a8746094642
Yongmin0423 <yym21345@gmail.com>
2025-06-14 23:03:02 +0900

    [#101] feat: 리뷰 없을 때 화면 구현

--- src/app/wines/[wineId]/page.tsx
+++ src/app/wines/[wineId]/page.tsx
@@ -6,6 +6,7 @@ import { privateInstance } from '@/apis/privateInstance';
 import dummyWineImage from '@/app/assets/images/dummy_wine_image.png';
 import WineCard from '@/app/myprofile/components/Card/WineCard';
 
+import Nothing from './components/Nothing';
 import ReviewCard from './components/ReviewCard';
 import ReviewOverview from './components/ReviewRating/ReviewOverview';
 import { REVIEW_RANGES } from './dummy';
@@ -35,23 +36,30 @@ export default function WinePage() {
         region={wineInfo?.region || 'Western Cape, South Africa'}
       />
 
-      <div className='flex flex-col gap-10 xl:grid xl:grid-cols-6'>
-        <div className='order-2 col-span-1 xl:order-1 xl:col-span-4'>
-          <h3 className='mb-10 text-xl font-bold'>리뷰 목록</h3>
-          <div className='flex flex-col gap-5'>
-            {wineInfo?.reviews &&
-              wineInfo?.reviews.map((review) => (
+      <div className='flex w-full flex-col gap-10 xl:grid xl:grid-cols-6'>
+        <div className='order-2 col-span-1 w-full xl:order-1 xl:col-span-4'>
+          <h3 className='mt-10 mb-10 text-xl font-bold'>리뷰 목록</h3>
+          {wineInfo?.reviews && wineInfo?.reviews.length > 0 ? (
+            <div className='flex flex-col gap-5'>
+              {wineInfo?.reviews.map((review) => (
                 <ReviewCard key={review.id} review={review} />
               ))}
+            </div>
+          ) : (
+            <div className='mt-10'>
+              <Nothing />
+            </div>
+          )}
+        </div>
+        {wineInfo?.reviews && wineInfo?.reviews.length > 0 && (
+          <div className='order-1 col-span-1 xl:order-2 xl:col-span-2'>
+            <ReviewOverview
+              data={REVIEW_RANGES}
+              rating={4.8}
+              reviewNumber={totalReviews}
+            />
           </div>
-        </div>
-        <div className='order-1 col-span-1 xl:order-2 xl:col-span-2'>
-          <ReviewOverview
-            data={REVIEW_RANGES}
-            rating={4.8}
-            reviewNumber={totalReviews}
-          />
-        </div>
+        )}
       </div>
     </div>
   );
