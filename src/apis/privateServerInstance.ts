5bb17cbf05b318a2883285c31cf1339292f27881
김서연 <rainbow2870@naver.com>
2025-06-15 19:42:17 +0900

    [#111] fix: 서버 컴포넌트에서 /api/auth/reftresh 호출 시 next_public_api_server_url 대신 site_url 사용

--- src/apis/privateServerInstance.ts
+++ src/apis/privateServerInstance.ts
@@ -40,7 +40,7 @@ export const createPrivateServerInstance = async (): Promise<AxiosInstance> => {
 
       //쿠키를 직접헤더에 넣음
       const res = await axios.post(
-        `${process.env.NEXT_PUBLIC_SITE_URL}/api/auth/refresh`,
+        `${process.env.SITE_URL}/api/auth/refresh`,
         {},
         {
           headers: {
