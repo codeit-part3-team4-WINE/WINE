5fd55ed90a1ea2afcd07233dea715a2fa4a08972
김서연 <rainbow2870@naver.com>
2025-06-16 03:35:21 +0900

    [#111] refactor: 프로필 섹션 PC에서만 sticky 적용되도록 수정

--- src/app/myprofile/layout.tsx
+++ src/app/myprofile/layout.tsx
@@ -8,7 +8,7 @@ export default async function MypageLayout({
   return (
     <main className='relative mx-[1.6rem] mt-7 max-w-[120rem] md:mx-[2rem] lg:mx-[2rem] xl:mx-[auto]'>
       <div className='flex w-full flex-col xl:flex-row xl:gap-[5rem]'>
-        <div className='sticky top-[4rem] h-fit'>
+        <div className='xl:sticky xl:top-[4rem] xl:h-fit'>
           <ProfileSection />
         </div>
         <div className='flex w-full flex-col'>{children}</div>
