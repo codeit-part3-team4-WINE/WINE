a76014253668145548c7ba760ae0c5cfabf4c662
김서연 <rainbow2870@naver.com>
2025-06-15 17:00:17 +0900

    [#111] feat: 리뷰 api 연동

--- src/app/myprofile/(tab)/reviews/page.tsx
+++ src/app/myprofile/(tab)/reviews/page.tsx
@@ -1,19 +1,37 @@
+/* eslint-disable */
+import { createPrivateServerInstance } from '@/apis/privateServerInstance';
 import ReviewCard from '../../components/Card/ReviewCard';
-import { reviews, wines } from '../../components/mock/mock-data';
+import Link from 'next/link';
 
-export default function Reviews() {
-  const review = reviews.list[0];
-  const wine = wines.list[0];
+export default async function Reviews() {
+  const axios = await createPrivateServerInstance();
+
+  const { data } = await axios.get('/users/me/reviews', {
+    params: {
+      limit: 10,
+    },
+  });
+
+  console.log(data.list[0]['wine']);
+
+  const reviews = data.list;
+
+  if (!Array.isArray(reviews) || reviews.length === 0) {
+    return <div className='text-gray-500'>작성된 리뷰가 없습니다.</div>;
+  }
+
   return (
     <div className='flex flex-col gap-10'>
-      {Array.from({ length: 10 }).map((_, index) => (
-        <ReviewCard
-          key={index}
-          content={review.content}
-          rating={review.rating}
-          time={review.updatedAt}
-          wine={wine.name}
-        />
+      {reviews.map((review: any) => (
+        <Link key={review.id} href={`/wines/${review.wine.id}`}>
+          <ReviewCard
+            key={review.id}
+            content={review.content}
+            rating={review.rating}
+            time={review.updatedAt}
+            wine={review.wine.name}
+          />
+        </Link>
       ))}
     </div>
   );
--- src/app/myprofile/(tab)/wines/page.tsx
+++ src/app/myprofile/(tab)/wines/page.tsx
@@ -1,13 +1,27 @@
+/* eslint-disable */
+import { createPrivateServerInstance } from '@/apis/privateServerInstance';
 import WineCard from '../../components/Card/WineCard';
-import { wines } from '../../components/mock/mock-data';
 
-export default function Wines() {
-  const wine = wines.list[0];
+export default async function Wines() {
+  const axios = await createPrivateServerInstance();
+
+  const { data } = await axios.get('/users/me/wines', {
+    params: {
+      limit: 10,
+    },
+  });
+
+  const wines = data.list;
+
+  if (!Array.isArray(wines) || wines.length === 0) {
+    return <div className='text-gray-500'>등록된 와인이 없습니다.</div>;
+  }
+
   return (
     <div className='flex flex-col gap-10'>
-      {Array.from({ length: 10 }).map((_, index) => (
+      {wines.map((wine: any) => (
         <WineCard
-          key={index}
+          key={wine.id}
           className='cursor-pointer'
           image={wine.image}
           name={wine.name}
