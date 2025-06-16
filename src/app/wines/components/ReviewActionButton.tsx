6d1b3ea27f57b9523e1990ce6e17dd8c4a24579d
김서연 <rainbow2870@naver.com>
2025-06-16 14:12:31 +0900

    [#110] feat: form 모달에 연결 및 wines 페이지 등록 버튼에 연결

--- src/app/wines/components/ReviewActionButton.tsx
+++ src/app/wines/components/ReviewActionButton.tsx
@@ -1,18 +1,23 @@
 'use client';
 
+import { useState } from 'react';
+
 import Button from '@/components/Button';
+import WineModal from '@/components/Modals/WineModal/WineModal';
 
 export default function ReviewActionButton() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <section className='fixed bottom-0 left-0 z-20 flex h-[8rem] w-full items-center justify-center border-t-1 border-gray-100 bg-white md:relative md:z-auto md:col-start-3 md:col-end-4 md:row-start-2 md:row-end-3 md:h-full md:w-[20rem] md:border-none xl:hidden'>
       <Button
         className='h-[5rem] w-[90vw] md:h-full md:w-full'
         round='rounded'
         variant='primary'
-        onClick={() => console.log('button')}
+        onClick={() => setIsOpen(true)}
       >
         와인 등록하기
       </Button>
+      <WineModal isOpen={isOpen} setIsOpen={setIsOpen} />
     </section>
   );
 }
--- src/components/Modals/WineModal/WineModal.tsx
+++ src/components/Modals/WineModal/WineModal.tsx
@@ -1,1 +1,44 @@
-export default function WineModal() {}
+import Button from '@/components/Button';
+import Modal from '@/components/Modal/Modal';
+import ModalBody from '@/components/Modal/ModalBody';
+import ModalClose from '@/components/Modal/ModalClose';
+import ModalContent from '@/components/Modal/ModalContent';
+import ModalFooter from '@/components/Modal/ModalFooter';
+import ModalHeader from '@/components/Modal/ModalHeader';
+import ModalTitle from '@/components/Modal/ModalTitle';
+
+import WineForm from './WineForm';
+
+export default function WineModal({
+  isOpen,
+  setIsOpen,
+}: {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  return (
+    <Modal externalIsOpen={isOpen} onExternalChange={setIsOpen}>
+      <ModalContent>
+        <ModalHeader>
+          <ModalTitle>와인 등록</ModalTitle>
+        </ModalHeader>
+        <ModalBody>
+          <WineForm />
+        </ModalBody>
+        <ModalFooter>
+          <ModalClose />
+          <ModalClose asChild>
+            <Button variant='secondary' onClick={() => {}}>
+              취소
+            </Button>
+          </ModalClose>
+          <ModalClose asChild>
+            <Button variant='primary' onClick={() => {}}>
+              등록하기
+            </Button>
+          </ModalClose>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
