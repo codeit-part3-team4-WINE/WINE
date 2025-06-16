15a49bcfcfe9ac570ffdab91cbbbc9b1b96fb797
MyungJiwoo <1206jiwoo@gmail.com>
2025-06-16 14:05:54 +0900

    [#114] refactor: 와인 카드에 가격 prop 전달 후 반영

--- src/app/wines/components/WineListContainer.tsx
+++ src/app/wines/components/WineListContainer.tsx
@@ -1,21 +1,22 @@
-import dummyWineImage from '@/app/assets/images/dummy_wine_image.png';
-
 import WineListCard from './WineListCard';
 
-export default function WineListContainer() {
+export default function WineListContainer({ data }) {
   return (
     <section className='col-start-1 col-end-4 row-start-3 row-end-5 flex flex-col gap-6 xl:col-start-2 xl:col-end-4 xl:row-start-3 xl:row-end-5'>
-      {Array.from({ length: 10 }, (_, index) => (
+      {data?.map((item) => (
         <WineListCard
-          key={index}
-          country='Western Cape, South Africa'
-          id={index + 1}
-          image={dummyWineImage}
-          name='Sentinel Carbernet Sauvignon 2016'
-          rating={4.8}
-          recentReview='Cherry, cocoa, vanilla and clove - beautiful red fruit driven Amarone.
-      Low acidity and medium tannins. Nice long velvety finish.'
-          reviewCount={47}
+          key={item.id}
+          country={item.region}
+          id={item.id}
+          image={item.image}
+          name={item.name}
+          price={item.price}
+          rating={item.avgRating}
+          recentReview={
+            item.recentReview?.content ??
+            '최신 후기가 없습니다. 후기를 남겨주세요!'
+          }
+          reviewCount={item.reviewCount}
         />
       ))}
     </section>
