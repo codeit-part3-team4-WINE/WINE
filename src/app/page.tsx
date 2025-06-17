8e17abf8954625e4e9018eac7c193b3f763c5512
김태일 <ejrrhcl56@naver.com>
2025-06-18 02:47:30 +0900

    [#116] feat: 코드 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 'use client';
+
 import { useRouter } from 'next/navigation';
 
 import Button from '@/components/Button';
 
 export default function LandingPage() {
   const router = useRouter();
+
   return (
     <div>
       <h1 className='text-primary-100 text-3xl'>Landing</h1>
-      <Button onClick={() => router.push('/design-system')}>
-        go to design system
-      </Button>
+
+      <div className='mt-4 flex flex-col gap-4'>
+        <Button onClick={() => router.push('/design-system')}>
+          go to design system
+        </Button>
+      </div>
     </div>
   );
 }
