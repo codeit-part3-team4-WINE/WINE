35bf4039312fa9cfafe379d85a6e387efde443ca
김서연 <rainbow2870@naver.com>
2025-06-18 15:16:09 +0900

    [#105] chore: layout.tsx 변경 & 불필요한 디자인 시스템 페이지 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css';
 
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
 import QueryProvider from '@/libs/queryProvider';
+
+import DynamicLayout from './DynamicLayout';
 
 export default function RootLayout({
   children,
@@ -11,16 +11,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko'>
-      <body className='overflow-x-hidden'>
-        <header className='mx-auto w-full max-w-[350rem] px-[5vw] md:px-[10vw] xl:px-[15vw]'>
-          <Header />
-        </header>
-        <main className='mx-auto my-10 w-full max-w-[350rem] px-[5vw] md:px-[10vw] xl:px-[15vw]'>
-          <QueryProvider>{children}</QueryProvider>
-        </main>
-        <footer>
-          <Footer />
-        </footer>
+      <body>
+        <DynamicLayout>
+          <main className='mx-auto my-10 w-full max-w-[350rem] px-[5vw] md:px-[10vw] xl:px-[15vw]'>
+            <QueryProvider>{children}</QueryProvider>
+          </main>
+        </DynamicLayout>
         <div id='modal-root' />
       </body>
     </html>
