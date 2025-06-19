652fe54612bd7117180b55c2d6e1dddc95c3ea07
Yongmin0423 <yym21345@gmail.com>
2025-06-19 13:30:08 +0900

    [#135] feat: 무한 스크롤 시 더 불러올 때 스켈레톤 보이도록 추가

--- src/app/wines/[wineId]/page.tsx
+++ src/app/wines/[wineId]/page.tsx
@@ -5,7 +5,6 @@ import { useEffect, useState } from 'react';
 
 import { privateInstance } from '@/apis/privateInstance';
 import WineCard from '@/app/myprofile/components/Card/WineCard';
-import LoadingAnimation from '@/components/LoadingAnimation';
 import useIntersectionObserver from '@/hooks/useIntersectionObserver';
 import { cn } from '@/libs/cn';
 import useUserStore from '@/stores/Auth-store/authStore';
@@ -15,6 +14,7 @@ import FlavorAnalysis from './components/flavorAnalysis/FlavorAnalysis';
 import Nothing from './components/Nothing';
 import ReviewCard from './components/ReviewCard';
 import ReviewOverview from './components/ReviewRating/ReviewOverview';
+import ReviewCardSkeleton from './components/SkeletonUi/ReviewCardSkeleton';
 import WinePageSkeleton from './components/SkeletonUi/WinePageSkeleton';
 import { ReviewType } from './types';
 
@@ -28,7 +28,6 @@ export default function WinePage() {
       queryKey: ['wine', wineId],
       queryFn: async ({ pageParam = 1 }) => {
         // 스켈레톤 UI 확인용 지연
-        await new Promise((resolve) => setTimeout(resolve, 2000));
         return privateInstance.get(
           `/wines/${wineId}?page=${pageParam}&limit=5`,
         );
@@ -108,8 +107,9 @@ export default function WinePage() {
               ))}
 
               {hasNextPage && (
-                <div ref={observerRef} className='h-10'>
-                  <LoadingAnimation />
+                <div ref={observerRef} className='h-20'>
+                  <ReviewCardSkeleton />
+                  <ReviewCardSkeleton />
                 </div>
               )}
             </div>
